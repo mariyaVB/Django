f30c88747f1daa531267259c67f872de553f8403
mariyaVB <148524465+mariyaVB@users.noreply.github.com>
2024-04-22 14:36:53 +0500

    Delete Галерея directory

--- Галерея/my_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-import my_app.views as art
-
-urlpatterns = [
-    path('', art.info_main),
-    path('question_answer/', art.question_answer),
-    path('<slug:painting_slug>/', art.info_art),
-]
